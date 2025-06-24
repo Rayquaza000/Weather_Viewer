a19101a770b90714860acc45c8eea4712e0aa846
Rayquaza001 <mehtapratik2001@gmai.com>
2025-06-24 18:38:31 +0530

    structure using html and css code

---
+++ script.js
@@ -0,0 +1,6 @@
+const current_day_plus_one_div=document.getElementById("current_day_plus_one_div");
+const current_day_plus_two_div=document.getElementById("current_day_plus_two_div");
+const current_day_plus_three_div=document.getElementById("current_day_plus_three_div");
+const current_day_plus_four_div=document.getElementById("current_day_plus_four_div");
+const current_day_plus_five_div=document.getElementById("current_day_plus_five_div");
+const current_day_plus_six_div=document.getElementById("current_day_plus_six_div");
